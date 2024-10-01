4e461db209eef39c683dc5f5d1fa3c989b684fbb
vuongcdt <vuongcdt@gmail.com>
2024-10-01 17:56:53 +0700

    fix buy gun

--- assets/Scripts/GamePlay/AmmoTank.ts
+++ assets/Scripts/GamePlay/AmmoTank.ts
@@ -1,4 +1,4 @@
-import { _decorator, Collider2D, IPhysics2DContact } from 'cc';
+import { _decorator, BoxCollider2D, Collider2D, game, IPhysics2DContact } from 'cc';
 import { Ammo } from './Ammo';
 import { TowerPlacement } from './TowerPlacement';
 const { ccclass, property } = _decorator;
@@ -6,11 +6,16 @@ const { ccclass, property } = _decorator;
 @ccclass('AmmoTank')
 export class AmmoTank extends Ammo {
     onBeginContact(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
-        let target = otherCollider.node.getParent().getComponent(TowerPlacement);
+        let tower = otherCollider.node.getParent().getComponent(TowerPlacement);
 
-        if (target) {
-            target.setHP(this._damage);
+        if (tower) {
+            tower.setHP(this._damage);
         }
+        // if (otherCollider instanceof BoxCollider2D && tower){
+
+        //     game.pause();
+        //     tower.setHP(this._damage);
+        // }
     }
 }
 
