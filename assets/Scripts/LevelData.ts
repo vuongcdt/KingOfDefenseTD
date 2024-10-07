476980c09c4ecdd64d27ae151b98b0026d51fcc6
vuongcdt <vuongcdt@gmail.com>
2024-10-07 23:19:46 +0700

    clear code

--- assets/Scripts/LevelData.ts
+++ assets/Scripts/LevelData.ts
@@ -32,120 +32,120 @@ export const levels: LevelData[] = [
                     timeDelay: 1
                 }]
             },
-            // {
-            //     name: 'wave 2',
-            //     way: 0,
-            //     dataEnemy: [{
-            //         type: CharacterType.Soldier,
-            //         total: 3,
-            //         timeDelay: 1
-            //     },
-            //     {
-            //         type: CharacterType.Soldier,
-            //         total: 3,
-            //         timeDelay: 2
-            //     }]
-            // },
-            // {
-            //     name: 'wave 3',
-            //     way: 0,
-            //     dataEnemy: [{
-            //         type: CharacterType.Tank,
-            //         total: 1,
-            //         timeDelay: 1
-            //     },
-            //     {
-            //         type: CharacterType.Soldier,
-            //         total: 3,
-            //         timeDelay: 3
-            //     }]
-            // },
-            // {
-            //     name: 'wave 4',
-            //     way: 0,
-            //     dataEnemy: [{
-            //         type: CharacterType.Tank,
-            //         total: 3,
-            //         timeDelay: 1
-            //     },
-            //     {
-            //         type: CharacterType.Soldier,
-            //         total: 3,
-            //         timeDelay: 3
-            //     },
-            //     {
-            //         type: CharacterType.Soldier,
-            //         total: 3,
-            //         timeDelay: 4
-            //     }]
-            // },
-            // {
-            //     name: 'wave 5',
-            //     way: 0,
-            //     dataEnemy: [{
-            //         type: CharacterType.Tank,
-            //         total: 3,
-            //         timeDelay: 1
-            //     },
-            //     {
-            //         type: CharacterType.Soldier,
-            //         total: 3,
-            //         timeDelay: 3
-            //     },
-            //     {
-            //         type: CharacterType.Plane,
-            //         total: 3,
-            //         timeDelay: 4
-            //     }]
-            // },
-            // {
-            //     name: 'wave 6',
-            //     way: 0,
-            //     dataEnemy: [{
-            //         type: CharacterType.Tank,
-            //         total: 3,
-            //         timeDelay: 1
-            //     },
-            //     {
-            //         type: CharacterType.Tank,
-            //         total: 3,
-            //         timeDelay: 3
-            //     },
-            //     {
-            //         type: CharacterType.Soldier,
-            //         total: 3,
-            //         timeDelay: 5
-            //     },
-            //     {
-            //         type: CharacterType.Soldier,
-            //         total: 3,
-            //         timeDelay: 6
-            //     },
-            //     {
-            //         type: CharacterType.Plane,
-            //         total: 3,
-            //         timeDelay: 6
-            //     }]
-            // },
-            // {
-            //     name: 'wave 7',
-            //     way: 0,
-            //     dataEnemy: [{
-            //         type: CharacterType.Plane,
-            //         total: 3,
-            //         timeDelay: 1
-            //     },
-            //     {
-            //         type: CharacterType.Plane,
-            //         total: 3,
-            //         timeDelay: 3
-            //     },
-            //     {
-            //         type: CharacterType.Plane,
-            //         total: 3,
-            //         timeDelay: 5
-            //     }]
-            // },
+            {
+                name: 'wave 2',
+                way: 0,
+                dataEnemy: [{
+                    type: CharacterType.Soldier,
+                    total: 3,
+                    timeDelay: 1
+                },
+                {
+                    type: CharacterType.Soldier,
+                    total: 3,
+                    timeDelay: 2
+                }]
+            },
+            {
+                name: 'wave 3',
+                way: 0,
+                dataEnemy: [{
+                    type: CharacterType.Tank,
+                    total: 1,
+                    timeDelay: 1
+                },
+                {
+                    type: CharacterType.Soldier,
+                    total: 3,
+                    timeDelay: 3
+                }]
+            },
+            {
+                name: 'wave 4',
+                way: 0,
+                dataEnemy: [{
+                    type: CharacterType.Tank,
+                    total: 3,
+                    timeDelay: 1
+                },
+                {
+                    type: CharacterType.Soldier,
+                    total: 3,
+                    timeDelay: 3
+                },
+                {
+                    type: CharacterType.Soldier,
+                    total: 3,
+                    timeDelay: 4
+                }]
+            },
+            {
+                name: 'wave 5',
+                way: 0,
+                dataEnemy: [{
+                    type: CharacterType.Tank,
+                    total: 3,
+                    timeDelay: 1
+                },
+                {
+                    type: CharacterType.Soldier,
+                    total: 3,
+                    timeDelay: 3
+                },
+                {
+                    type: CharacterType.Plane,
+                    total: 3,
+                    timeDelay: 4
+                }]
+            },
+            {
+                name: 'wave 6',
+                way: 0,
+                dataEnemy: [{
+                    type: CharacterType.Tank,
+                    total: 3,
+                    timeDelay: 1
+                },
+                {
+                    type: CharacterType.Tank,
+                    total: 3,
+                    timeDelay: 3
+                },
+                {
+                    type: CharacterType.Soldier,
+                    total: 3,
+                    timeDelay: 5
+                },
+                {
+                    type: CharacterType.Soldier,
+                    total: 3,
+                    timeDelay: 6
+                },
+                {
+                    type: CharacterType.Plane,
+                    total: 3,
+                    timeDelay: 6
+                }]
+            },
+            {
+                name: 'wave 7',
+                way: 0,
+                dataEnemy: [{
+                    type: CharacterType.Plane,
+                    total: 3,
+                    timeDelay: 1
+                },
+                {
+                    type: CharacterType.Plane,
+                    total: 3,
+                    timeDelay: 3
+                },
+                {
+                    type: CharacterType.Plane,
+                    total: 3,
+                    timeDelay: 5
+                }]
+            },
         ]
     },
     {
@@ -249,12 +249,12 @@ export const levels: LevelData[] = [
                     total: 3,
                     timeDelay: 8.5
                 },
-                // {
-                //     type: CharacterType.Plane,
-                //     total: 3,
-                //     timeDelay: 1
-                // }
-            ]
+                    // {
+                    //     type: CharacterType.Plane,
+                    //     total: 3,
+                    //     timeDelay: 1
+                    // }
+                ]
             },
             {
                 name: 'wave 6',
@@ -284,7 +284,7 @@ export const levels: LevelData[] = [
                     total: 3,
                     timeDelay: 1
                 }
-            ]
+                ]
             },
             // {
             //     name: 'wave 7',
